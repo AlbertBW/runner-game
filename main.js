12f17f9f98da7cfff4488ea8bc67a03dd3de34f2
Albert Wales <41163950+AlbertBW@users.noreply.github.com>
2024-01-01 10:26:36 +0000

    Game over, timer, particles, collisions, states, UI

--- main.js
+++ main.js
@@ -22,14 +22,24 @@ window.addEventListener("load", function () {
       this.input = new InputHandler(this);
       this.ui = new UI(this);
       this.enemies = [];
+      this.particles = [];
+      this.collisions = [];
+      this.maxParticles = 200;
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
-      this.debug = true;
+      this.debug = false;
       this.score = 0;
       this.fontColor = "black";
+      this.time = 0;
+      this.maxTime = 10000;
+      this.gameOver = false;
+      this.player.currentState = this.player.states[0];
+      this.player.currentState.enter();
     }
 
     update(deltaTime) {
+      this.time += deltaTime;
+      if (this.time > this.maxTime) this.gameOver = true;
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
       // handleEnemies
@@ -45,6 +55,19 @@ window.addEventListener("load", function () {
           this.enemies.splice(this.enemies.indexOf(enemy), 1);
         }
       });
+      // handle particles
+      this.particles.forEach((particle, index) => {
+        particle.update();
+        if (particle.markedForDeletion) this.particles.splice(index, 1);
+      });
+      if (this.particles.length > this.maxParticles) {
+        this.particles.length = this.maxParticles;
+      }
+      // handle collision sprites
+      this.collisions.forEach((collision, index) => {
+        collision.update(deltaTime);
+        if (collision.markedForDeletion) this.collisions.splice(index, 1);
+      });
     }
 
     draw(context) {
@@ -53,6 +76,12 @@ window.addEventListener("load", function () {
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+      this.particles.forEach((particle) => {
+        particle.draw(context);
+      });
+      this.collisions.forEach((collision) => {
+        collision.draw(context);
+      });
       this.ui.draw(context);
     }
     addEnemy() {
@@ -60,12 +89,10 @@ window.addEventListener("load", function () {
         this.enemies.push(new GroundEnemy(this));
       else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
       this.enemies.push(new FlyingEnemy(this));
-      console.log(this.enemies);
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  console.log(game);
   let lastTime = 0;
 
   function animate(timeStamp) {
@@ -74,7 +101,7 @@ window.addEventListener("load", function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.update(deltaTime);
     game.draw(ctx);
-    requestAnimationFrame(animate);
+    if (!game.gameOver) requestAnimationFrame(animate);
   }
   animate(0);
 });
